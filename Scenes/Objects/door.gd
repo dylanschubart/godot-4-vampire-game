cde834c0b3b0b4e7a85d808335c4f2d9d930d36c
Joeri Neven <Joeri.Neven@ipggroup.eu>
2023-08-13 10:10:01 +0200

    Door not interactable on start -> only after animation finish

--- Scenes/Objects/door.gd
+++ Scenes/Objects/door.gd
@@ -2,11 +2,18 @@ extends Area2D
 
 #Setup the animation body
 @onready var _animation_player = $"../DoorTexture/AnimationPlayer"
+@onready var _collision = $"CollisionShape2D"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed('DebugInteract'):
 		_animation_player.play('Open')
-
+		
 func interact():
 	return 'hubSelect'
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == 'Open':
+		print(_collision.disabled)
+		_collision.disabled = false;
+		
