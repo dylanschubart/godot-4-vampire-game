886f927ad1163079da6f3ee905f9dc559095ad59
Diyllan <dr.manican@gmail.com>
2023-08-29 17:26:48 +0200

    level_end animation implemented

--- Scenes/Objects/Level_End_Zone.gd
+++ Scenes/Objects/Level_End_Zone.gd
@@ -1,4 +1,9 @@
 extends Area2D
 
+@onready var _animation_player = $"../Sprite2D/AnimationPlayer"
+
+func _ready():
+	_animation_player.play("Level_End_Idle")
+	
 func interact():
 	return "levelEnd"
